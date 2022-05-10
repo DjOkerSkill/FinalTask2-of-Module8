88efecd4d9c38ca366361d4ac942bf7be956b491
Роман <ram23@mail.ru>
2022-05-10 18:54:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+
+        public static long DirSize(DirectoryInfo di)
+        {
+            long size = 0;
+
+            FileInfo[] fl = di.GetFiles();
+
+            foreach (FileInfo fi in fl)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dr = di.GetDirectories();
+
+            foreach (DirectoryInfo df in dr)
+            {
+                size += DirSize(df);
+            }
+            return size;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string URLdirectory = "C:/C#.NET учеба";
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(URLdirectory);
+
+                long ResultSize = DirSize(directoryInfo);
+
+                Console.WriteLine($"Размер папки {URLdirectory} составляет: {ResultSize} байт");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
